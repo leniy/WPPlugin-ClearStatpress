675ce14f5fe0203a9d24d841647bf2ccd1f13664
Leniy Tsan <m@leniy.org>
2012-12-13 04:40:50 +0000

    First version after start learned how to write a plugin.
    git-svn-id: http://plugins.svn.wordpress.org/clear-statpress/trunk@638260 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Clear-StatPress.php
@@ -0,0 +1,114 @@
+<?php
+/*
+	Plugin Name: Clear-StatPress
+	Plugin URI: http://leniy.info/clear-statpress.html
+	Description: 我安装的statpress插件，虽然设置了不记录蜘蛛访问记录，但有些不表明身份的蜘蛛的访问数据仍然会保存，严重占用数据库空间。登陆后台phpmyadmin太麻烦了，刚刚粗略的学习了下插件制作，写了个简单的插件，执行清理作业。
+	Version: 0.0.1
+	Author: leniy
+	Author URI: http://leniy.info/
+*/
+
+/*  Copyright 2012 Leniy (m@leniy.info)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action('admin_menu', 'qw_CSP_menu');
+
+function qw_CSP_menu() {
+	add_options_page('Clear-StatPress', 'Clear-StatPress', 'administrator', 'Clear-StatPress.php', 'qw_CSP_page');
+}
+
+function qw_CSP_page() {
+	echo "<h2>Clear-StatPress</h2>";
+	CSP_sql_del();
+}
+
+function CSP_sql_del() {
+	global $wpdb;
+	$query1 = "SELECT* FROM " . $wpdb->prefix . 'statpress '." WHERE
+    `urlrequested` LIKE  '%/page%'
+OR  `urlrequested` LIKE  '%/tag%'
+OR  `urlrequested` LIKE  '%/category%'
+OR  `urlrequested` LIKE  '%/2006%'
+OR  `urlrequested` LIKE  '%/2007%'
+OR  `urlrequested` LIKE  '%/2008%'
+OR  `urlrequested` LIKE  '%/2009%'
+OR  `urlrequested` LIKE  '%/2010%'
+OR  `urlrequested` LIKE  '%/2011%'
+OR  `urlrequested` LIKE  '%/2012%'
+OR  `urlrequested` LIKE  '%/2013%'
+OR  `agent` LIKE  '%bot%'
+OR ( `urlrequested` LIKE  '' AND  `statuscode` LIKE  '' AND  `referrer` LIKE  '')
+";
+	$query2 = "DELETE FROM " . $wpdb->prefix . 'statpress '." WHERE
+    `urlrequested` LIKE  '%/page%'
+OR  `urlrequested` LIKE  '%/tag%'
+OR  `urlrequested` LIKE  '%/category%'
+OR  `urlrequested` LIKE  '%/2006%'
+OR  `urlrequested` LIKE  '%/2007%'
+OR  `urlrequested` LIKE  '%/2008%'
+OR  `urlrequested` LIKE  '%/2009%'
+OR  `urlrequested` LIKE  '%/2010%'
+OR  `urlrequested` LIKE  '%/2011%'
+OR  `urlrequested` LIKE  '%/2012%'
+OR  `urlrequested` LIKE  '%/2013%'
+OR  `agent` LIKE  '%bot%'
+OR ( `urlrequested` LIKE  '' AND  `statuscode` LIKE  '' AND  `referrer` LIKE  '')
+";
+	$output = $wpdb->get_results($query1);
+	echo "找到如下待删除项目：<br>";
+	echo "<div class=\"datagrid\"><table>
+	<thead><tr>
+	<th>id</th>
+	<th>date</th>
+	<th>time</th>
+	<th>ip</th>
+	<th>urlrequested</th>
+	<th>statuscode</th>
+	<th>agent</th>
+	<th>referrer</th>
+	<th>search</th>
+	<th>nation</th>
+	</tr></thead>
+
+	<tbody>";
+	$temptemp = 0;//用来使表格两行背景颜色不同
+	foreach ($output as $o) {
+		if($temptemp == 0) { echo "<tr>"; $temptemp = 1;}
+		else { echo "<tr class=\"alt\">"; $temptemp = 0;}
+		echo "<td>" . $o->id . "</td>";
+		echo "<td>" . $o->date . "</td>";
+		echo "<td>" . $o->time . "</td>";
+		echo "<td>" . $o->ip . "</td>";
+		echo "<td>" . $o->urlrequested . "</td>";
+		echo "<td>" . $o->statuscode . "</td>";
+		echo "<td>" . $o->agent . "</td>";
+		echo "<td>" . $o->referrer . "</td>";
+		echo "<td>" . $o->search . "</td>";
+		echo "<td>" . $o->nation . "</td>";
+		echo "</tr>";
+	}
+	echo "</tbody></table></div>";
+?>
+<style text="text/css">
+.datagrid table { border-collapse: collapse; text-align: left; width: 100%; } .datagrid {font: normal 12px/150% Arial, Helvetica, sans-serif; background: #fff; overflow: hidden; border: 1px solid #006699; -webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; }.datagrid table td, .datagrid table th { padding: 0px 5px; }.datagrid table thead th {background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #006699), color-stop(1, #00557F) );background:-moz-linear-gradient( center top, #006699 5%, #00557F 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#006699', endColorstr='#00557F');background-color:#006699; color:#FFFFFF; font-size: 15px; font-weight: bold; border-left: 1px solid #0070A8; } .datagrid table thead th:first-child { border: none; }.datagrid table tbody td { color: #00557F; border-left: 2px solid #E1EEF4;font-size: 12px;font-weight: normal; }.datagrid table tbody .alt td { background: #E1EEF4; color: #00557F; }.datagrid table tbody td:first-child { border-left: none; }.datagrid table tbody tr:last-child td { border-bottom: none; }
+</style>
+<?php
+	$wpdb->query($query2);
+	echo "<br>删除成功";
+
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+﻿=== Clear-StatPress ===
+Contributors: leniy
+Donate link: http://leniy.info/
+Tags: comments,stat,statpress,clear
+Requires at least: 3.0.1
+Tested up to: 3.4.2
+Stable trunk: 0.0.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Clear-StatPress:Clear statpress spider mysql log data
+
+== Description ==
+
+我安装的statpress插件，虽然设置了不记录蜘蛛访问记录，但有些不表明身份的蜘蛛的访问数据仍然会保存，严重占用数据库空间。
+登陆后台phpmyadmin太麻烦了，刚刚粗略的学习了下插件制作，写了个简单的插件，执行清理作业。
+
+== Installation ==
+
+1. Copy Clear-StatPress.zip to your Wordpress plugins directory, usually `wp-content/plugins/` and unzip the file.  It will create a `wp-content/plugins/Clear-StatPress/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Follow the Clear-StatPress page under options.
+4. Then, just follow the Clear-StatPress page under options.
+
+== Screenshots ==
+
+1. screenshot-1.png
+
+== Changelog ==
+
+=0.0.1=
+First version after start learned how to write a plugin.
