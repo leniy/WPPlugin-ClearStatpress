fe49dc0e566d71762b4a9f05a7bb411954c0269f
Leniy Tsan <m@leniy.org>
2013-07-16 00:27:56 +0000

    Add some regulations
    git-svn-id: http://plugins.svn.wordpress.org/clear-statpress/trunk@741360 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Clear-StatPress.php
+++ Clear-StatPress.php
@@ -3,7 +3,7 @@
 	Plugin Name: Clear StatPress
 	Plugin URI: http://blog.leniy.org/clear-statpress.html
 	Description: 我安装的statpress插件，虽然设置了不记录蜘蛛访问记录，但有些不表明身份的蜘蛛的访问数据仍然会保存，严重占用数据库空间。登陆后台phpmyadmin太麻烦了，刚刚粗略的学习了下插件制作，写了个简单的插件，执行清理作业。
-	Version: 1.3.11
+	Version: 1.3.12
 	Author: leniy
 	Author URI: http://blog.leniy.org/
 	Text Domain: leniylang
@@ -108,6 +108,7 @@ function CSP_sql_del() {
 	OR `ip` LIKE '220.181.51.%'
 	OR `ip` LIKE '61.135.186.%'
 	OR `ip` LIKE '60.211.191.214'
+	OR `ip` LIKE '88.126.248.44'
 ";
 //然后生成查询和删除的完整语句
 	$query_sel = "SELECT* FROM " . $wpdb->prefix . 'statpress '." WHERE " . $query_temp;
